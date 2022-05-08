49c2d762dceb27d71381f12ce054c30955987e0a
Codefident <klep.piotr01@gmail.com>
2022-05-08 13:27:03 +0200

    provided new solution, W.I.P.

--- offline6/zad6.py
+++ offline6/zad6.py
@@ -7,9 +7,10 @@ from collections import deque
 class Vertex:
     def __init__(self, i):
         self.i = i   # number/index of vertex
-        self.d = []   # distance from vertex s
+        self.d = -1   # distance from vertex s
         self.parents = []   # parents vertex
         self.visited = False
+        self.lastVisited = -1
 
 
 def BFS(G, s, t):
@@ -21,34 +22,42 @@ def BFS(G, s, t):
     for i in range(e):
         V.append(Vertex(i))
 
-    V[s].d.append(0)
+    V[s].d = 0
     V[s].visited = True
     Q.appendleft(s)
 
-    while Q:
+    shortest_path_length = -1
+
+    while Q and shortest_path_length < 0:
         u = Q.popleft()
+
         for v in G[u]:
 
-            if not V[v].visited:
-                V[v].visited = True
-                V[v].d.append(V[u].d[0] + 1)
+            # najkrótsze ścieżki spotykają się
+            if V[v].visited and V[u].d + 1 == V[v].d:
                 V[v].parents.append(u)
 
+            # jesteśmy na dłuższej ścieżce
+            elif not V[v].visited:
+                V[v].d = V[u].d + 1
+                V[v].parents.append(u)
+                V[v].visited = True
+                    
                 if v != t:
                     Q.append(v)
-
-            else:
-                V[v].d.append(V[u].d[0] + 1)
-                V[v].parents.append(u)
-
+                else:
+                    shortest_path_length = V[v].d
+                
+    print()
     if V[t].visited:
-        i = t
-        while i != s:
-            if len(V[i].parents) > 1:
-                for d in V[i].d:
-                    if d > V[i].d[0]:
-                        return (i, V[i].parents[0])
-            i = V[i].parents[0]
+        return None
+        # i = t
+        # while i != s:
+        #     if len(V[i].parents) > 1:
+        #         for d in V[i].d:
+        #             if d > V[i].d[0]:
+        #                 return (i, V[i].parents[0])
+        #     i = V[i].parents[0]
     else:
         return None
 
