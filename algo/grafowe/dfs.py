6c854fb25026aaea1ce5df6064adac40bed6b97d
Codefident <klep.piotr01@gmail.com>
2022-05-11 19:13:49 +0200

    added dfs

---
+++ algo/grafowe/dfs.py
@@ -0,0 +1,16 @@
+def DFS(G):
+    n = len(G)
+    visited = [False] * n
+    parents = [None] * n
+    time = 0
+
+    for v in range(n):
+        if not visited[v]:
+            DFSVisit(G, v)
+
+    def DFSVisit(G: list, v: int):
+        nonlocal time
+        time += 1
+        for u in G[v]:
+            if not visited[u]:
+                DFSVisit(G, u)
